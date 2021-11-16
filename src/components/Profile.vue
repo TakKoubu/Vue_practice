a26c61ec7f7e7418219d24c9c43308c687cdff45
TakKokubu <kokubu.tk@gmail.com>
2021-11-16 17:47:11 +0900

    profileにおけるボタンの作成

---
+++ src/components/Profile.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class='profile'>
+    <div class="row justify-center mt-5">
+      <div class="col-md-6 col-12">
+        <v-avatar size="350" width="85%">
+          <img
+            src="../assets/img/cat.jpg"
+            alt="John"
+          >
+        </v-avatar>
+      </div>
+      <div class="col-md-6 col-12">
+        <v-card
+          class="mx-auto pa-4"
+          height="100%"
+        >
+          <v-list-item three-line>
+            <v-list-item-content>
+              <v-list-item-title class="text-h5 pb-2">
+                <div>こくぶ</div>
+              </v-list-item-title>
+              <v-divider></v-divider>
+              <v-card-text>
+                <div class="body-1 mb-1">
+                  IT技術に関心を持ち、プログラミングを学ぶ。学ぶ過程でイメージが形になる瞬間が自分自身の大きなモチベーションになることに気付き、WEB系エンジニアを目指すようになる。
+                  現在は税務関係の組織で働きながら、学習を継続中。
+                </div>
+              </v-card-text>
+              <v-divider></v-divider>
+            </v-list-item-content>
+          </v-list-item>
+           <v-card-actions>
+            <router-link to="/skill" tag="button">保有スキル</router-link>
+            <button @click="$router.push('work')">実績・制作物</button>
+          </v-card-actions>
+        </v-card>
+      </div>
+    </div>
+  </section>
+</template>
