ddc03c031bfb5345c9bec583f5a073be306685df
TakKokubu <kokubu.tk@gmail.com>
2021-11-17 17:06:36 +0900

    contactページのボタンの設定

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -9,7 +9,7 @@
           <v-list-item three-line>
             <v-list-item-content>
               <v-card-text>
-                <div class="body-1 mb-1 justify-center">
+                <div class="body-1 mb-1 text-center">
                   個人開発で得た知見や、学習したことについて発信していきます。<br>
                   フォローしていただけると励みになります。
                 </div>
@@ -17,7 +17,7 @@
             </v-list-item-content>
           </v-list-item>
           <v-card-actions>
-            <v-btn href="https://github.com/TakKoubu" color="white" border="none" background="transparent"><font-awesome-icon :icon="['fab', 'github-square']" size="3x" color="black"/>GITHUB</v-btn>
+            <v-btn text href="https://github.com/TakKoubu"><font-awesome-icon :icon="['fab', 'github-square']" size="3x" color="black"/>GITHUB</v-btn>
           </v-card-actions>
         </v-card>
       </div>
